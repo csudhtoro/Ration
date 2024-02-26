252f00ef4dfb34a876aee2dc396ed56814eab507
Dana Shorts <csudhtoro@hotmail.com>
2024-02-25 17:42:02 -0800

    edited link orientation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,24 +4,26 @@ const Footer = () => {
   return (
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 py-4">
       <div className="max-w-[1640px] m-auto px-4 py-4 w-full mx-auto p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Ration
-          </a>
-          . All Rights Reserved.
-        </span>
-        <div className="mx-auto flex items-center justify-start gap-3">
-          <p className="text-[0.65rem] text-gray-600">
-            Images provided by{" "}
-            <a
-              href="https://www.unsplash.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="text-blue-900 font-bold">Unsplash</span>{" "}
+        <div className="flex flex-col justify-center items-center">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" "}
+            <a href="https://flowbite.com/" className="hover:underline">
+              Ration
             </a>
-          </p>
+            . All Rights Reserved.
+          </span>
+          <div className="mx-auto flex items-center justify-start gap-3">
+            <p className="text-[0.65rem] text-gray-600">
+              Images provided by{" "}
+              <a
+                href="https://www.unsplash.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="text-blue-900 font-bold">Unsplash</span>{" "}
+              </a>
+            </p>
+          </div>
         </div>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
