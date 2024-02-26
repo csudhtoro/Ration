b5d47a7f97822fc619d25793511da44773f2cfc6
Dana Shorts <csudhtoro@hotmail.com>
2024-02-25 17:46:29 -0800

    align footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 py-4">
       <div className="max-w-[1640px] m-auto px-4 py-4 w-full mx-auto p-4 md:flex md:items-center md:justify-between">
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-start items-center">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
             <a href="https://flowbite.com/" className="hover:underline">
